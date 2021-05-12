aa36d6b592005966dc98e34d5c157c01b9eba1ca
tesw yew isal <278153+cnmade@users.noreply.github.com>
2021-05-12 17:00:46 +0800

    修正rpc代码逻辑

--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module github.com/cnmade/bsmi-mail-kernel
 
 go 1.16
 
-require github.com/json-iterator/go v1.1.11
+require (
+	github.com/StackExchange/wmi v0.0.0-20210224194228-fe8f1750fd46 // indirect
+	github.com/deckarep/golang-set v1.7.1 // indirect
+	github.com/ethereum/go-ethereum v1.10.3
+	github.com/go-ole/go-ole v1.2.5 // indirect
+	github.com/json-iterator/go v1.1.11
+	github.com/shirou/gopsutil v3.21.4+incompatible // indirect
+	golang.org/x/crypto v0.0.0-20210506145944-38f3c27a63bf // indirect
+	golang.org/x/sys v0.0.0-20210511113859-b0526f3d8744 // indirect
+	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce
+)
--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
-import "github.com/json-iterator/go"
+import (
+	"fmt"
+	"github.com/ethereum/go-ethereum/rpc"
+	"github.com/json-iterator/go"
+	"gopkg.in/natefinch/npipe.v2"
+	"net"
+)
+
+
+
+func newTestServer() *rpc.Server {
+	server := rpc.NewServer()
+	if err := server.RegisterName("test", new(testService)); err != nil {
+		panic(err)
+	}
+	return server
+}
+
+
+
+type testService struct{}
+
+type echoArgs struct {
+	S string
+}
+
+type echoResult struct {
+	Method string
+	String string
+	Int    int
+	Args   *echoArgs
+}
+
+func (s *testService) Echo(str string, i int, args *echoArgs) echoResult {
+	return echoResult{"test_echo", str, i, args}
+}
 func main() {
 	var json = jsoniter.ConfigCompatibleWithStandardLibrary
 	data := 1
@@ -11,4 +45,25 @@ func main() {
 	}
 	fmt.Printf("json: %+v", marshal)
 	fmt.Println("Hello world")
+
+
+	ln, err := npipe.Listen(`\\.\pipe\bsmi_mail_kernel`)
+	if err != nil {
+		// handle error
+	}
+	server := newTestServer()
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			// handle error
+			continue
+		}
+
+		go server.ServeCodec(rpc.NewCodec(conn), 0)
+	}
+
+}
+
+func handleConnection(conn net.Conn) {
+
 }
